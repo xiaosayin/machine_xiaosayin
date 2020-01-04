09a398a7b9b6b3d2338b18be8c6c9f66c92f2a5b
xiaosayin <811469225@qq.com>
2020-01-04 08:36:34 +0800

    mylibOfduducheng

--- mylib/models/misc.py
+++ mylib/models/misc.py
@@ -1,7 +1,7 @@
 import keras.backend as K
 import tensorflow as tf
 
-
+'''
 def get_gpu_session(ratio=None, interactive=False):
     config = tf.ConfigProto(allow_soft_placement=True)
     if ratio is None:
@@ -13,8 +13,16 @@ def get_gpu_session(ratio=None, interactive=False):
     else:
         sess = tf.Session(config=config)
     return sess
+'''
 
+def get_gpu_session():
+    config = tf.ConfigProto(device_count = {'cuda': 0})
+    config.gpu_options.allow_growth = True
+    config.gpu_options.per_process_gpu_memory_fraction = 0.7
+    sess = tf.Session(config=config)
+    return sess
+    
 
 def set_gpu_usage(ratio=None):
-    sess = get_gpu_session(ratio)
+    sess = get_gpu_session()
     K.set_session(sess)
